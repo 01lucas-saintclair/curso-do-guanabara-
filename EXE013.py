076de8aca1d1496375874c863f630bb177abc82f
01lucas-saintclair <lucas.nascimento.bruno@gmail.com>
2025-09-01 15:44:19 -0300

    Create EXE013.py

---
+++ EXE013.py
@@ -0,0 +1,5 @@
+print('qual e o salario do funcionario')
+salario = float(input())
+almento = salario * 0.15
+salario_novo = salario + almento
+print('salario do funcionario ganha {} depois do almento de 15% o salario se tornou {}'.format(salario, salario_novo))
