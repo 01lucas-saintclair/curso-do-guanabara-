5b6a9fa211f273c2c4b27e65a8c52faf86536541
01lucas-saintclair <lucas.nascimento.bruno@gmail.com>
2025-09-01 15:39:17 -0300

    Create EXE007.py

---
+++ EXE007.py
@@ -0,0 +1,7 @@
+print('digite a primeira nota do aluno')
+n1 = float(input())
+print('digite a segunda nota do aluno')
+n2 = float(input())
+s = n1 + n2
+m = s/2
+print('a media entre {} e {} e igual a {}'.format(n1, n2, m))
